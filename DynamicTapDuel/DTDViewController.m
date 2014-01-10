9aeeb27bc32617a1e973c522a20da606b7ca6b57
tylacock <tylacock@gmail.com>
2014-01-10 15:55:03 -0500

    Add second button

--- DynamicTapDuel/DTDViewController.m
+++ DynamicTapDuel/DTDViewController.m
@@ -38,6 +38,15 @@
         
         
         [button setCenter:CGPointMake(x, y)];
+    } else {
+        int xmin = ([button frame].size.width)/2;
+        int ymin = ([button frame].size.height)/2;
+        
+        int x = xmin + arc4random_uniform(self.view.frame.size.width - button.frame.size.width);
+//        int y = ymin + arc4random_uniform((self.view.frame.size.height) / 2 - button.frame.size.height);
+        int y = arc4random_uniform(50);
+        
+        [button setCenter:CGPointMake(x, y)];
     }
     
     
